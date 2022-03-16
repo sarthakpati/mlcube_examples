13ccf42b8cdabeef712de5df201ced1139074af0
sarthakpati <sarthak.pati@hotmail.com>
2022-03-16 11:01:12 -0400

    added seg

--- medperf/data_preparator/project/sanity_check.py
+++ medperf/data_preparator/project/sanity_check.py
@@ -14,7 +14,7 @@ def check_subject_validity(subject_dir):
         bool: True if the subject folder is valid, False otherwise.
     """
     subject_valid = True
-    strings_to_check = ["_t1.nii.gz", "_t1ce.nii.gz", "_t2.nii.gz", "_flair.nii.gz"]
+    strings_to_check = ["_t1.nii.gz", "_t1ce.nii.gz", "_t2.nii.gz", "_flair.nii.gz", "_seg.nii.gz"]
 
     for string in strings_to_check:
         if not os.path.isfile(os.path.join(subject_dir, os.path.basename(subject_dir) + string)):
@@ -40,7 +40,7 @@ def sanity_check(data_path):
         current_subject = os.path.join(data_path, folders)
         if os.path.isdir(current_subject):
 
-            assert check_subject_validity(current_subject), "Subject {} does not contain all modalities".format(current_subject)
+            assert check_subject_validity(current_subject), "Subject {} does not contain all modalities or segmentation".format(current_subject)
                 
             for files in os.listdir(current_subject):
                 current_file = os.path.join(current_subject, files)
@@ -53,7 +53,6 @@ def sanity_check(data_path):
 
                         assert (base_size==size_array).all(), "Image size is not [240,240,155] for " + current_file
                         assert (base_spacing==spacing_array).all(), "Image resolution is not [1,1,1] for " + current_file
-            
 
 
 if __name__ == '__main__':
