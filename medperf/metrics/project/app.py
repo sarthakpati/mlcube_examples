f40cb68e2ce572ee193e655d2816f760f34b4067
sarthakpati <sarthak.pati@hotmail.com>
2022-03-16 18:50:50 -0400

    added metric extraction logic from sage, and made edits to make it a bit more generic

--- medperf/metrics/project/app.py
+++ medperf/metrics/project/app.py
@@ -1,101 +1,77 @@
-# Hello World Script
-#
-# This script is unrelated to the MLCube interface. It could be run
-# independently without issues. It provides the actual implementation
-# of the metrics. This file is executed by MLCube through mlcube.py
+# Code adapted from https://github.com/Sage-Bionetworks-Challenges/brats-dream-challenge-infra/blob/main/Docker/score.py
+
 import argparse, os
-import yaml
-import SimpleITK as sitk
-import medpy.metric.binary.dc as dice
+import os
+import subprocess
+import argparse
 
-def check_subject_validity(subject_dir):
-    """Checks if a subject folder is valid.
-    Args:
-        subject_dir (str): The subject folder.
-    Returns:
-        bool: True if the subject folder is valid, False otherwise.
+import pandas as pd
+
+
+def run_captk(pred, gold, tmp):
     """
-    subject_valid = True
-    strings_to_check = ["_seg.nii.gz"]
-
-    for string in strings_to_check:
-        if not os.path.isfile(os.path.join(subject_dir, os.path.basename(subject_dir) + string)):
-            subject_valid = False
-            break
-    
-    return subject_valid
-
-
-
-def generate_metrics(gt, pred):
-    """Generates the metrics for the provided data and predictions
-    Args:
-        gt (str): File name of ground truth.
-        pred (str): File name of prediction.
-    Returns:
-        dict: dictionary containing key-value pairs for identified the labels-of-interest and the common identifier column.
+    Run BraTS Similarity Metrics computation of prediction scan
+    against goldstandard.
     """
-    results = {}
-    ## might make sense to wait for sage response
-    gt_array = sitk.GetArrayFromImage(sitk.ReadImage(gt))
-    pred_array = sitk.GetArrayFromImage(sitk.ReadImage(pred))
-    brats_labels_definitions = {"NET":"1", "ED":"2", "ET":"4", "TC":"1||4", "WT":"1||2||4"}
-
-    for key in brats_labels_definitions:
-        if "||" in brats_labels_definitions[key]:  # special case
-            class_split = brats_labels_definitions[key].split("||")
-            gt_array_split = gt_array == int(class_split[0])
-            pred_array_split = pred_array == int(class_split[0])
-            for i in range(1, len(class_split)):
-                gt_array_split = gt_array_split | (
-                    gt_array == int(class_split[i])
-                )
-                pred_array_split = pred_array_split | (
-                    pred_array == int(class_split[i])
-                )
-        else:
-            # assume that it is a simple int
-            gt_array_split = gt_array == int(brats_labels_definitions[key])
-            pred_array_split = pred_array == int(brats_labels_definitions[key])
-
-        results_dict = {}
-        results_dict["Dice"] = dice(gt_array_split, pred_array_split)
-
-    return results
+    cmd = [
+        os.path.join("/work/CaPTk/bin/Utilities"),
+        "-i", gold,
+        "-lsb", pred,
+        "-o", tmp
+    ]
+    subprocess.check_call(cmd)
 
 
-def generate_stats(data_path, preds_path):
-    """Generates the metrics for the provided data and predictions
-    Args:
-        data_path (str): The path to the data folder
-        preds_path (str): The path to the predictions folder
+def extract_metrics(tmp, subject_id):
+    """Get scores for three regions: ET, WT, and TC.
+    Metrics wanted:
+      - Dice score
+      - Hausdorff distance
+      - specificity
+      - sensitivity
+      - precision
     """
-    all_subjects_gt = os.listdir(data_path)
-    all_subjects_preds = os.listdir(preds_path)
+    res = (
+        pd.read_csv(tmp, index_col="Labels")
+        .filter(items=["Labels", "Dice", "Hausdorff95",
+                       "Sensitivity", "Specificity",
+                       "Precision"])
+        .filter(items=["ET", "WT", "TC"], axis=0)
+        .reset_index()
+        .assign(subject_id=subject_id)
+        .pivot(index="subject_id", columns="Labels")
+    )
+    res.columns = ["_".join(col).strip() for col in res.columns]
+    return res
 
-    for folders in all_subjects_gt:
-        current_subject = os.path.join(data_path, folders)
-        if os.path.isdir(current_subject):
 
-            assert check_subject_validity(current_subject), "Subject {} does not contain ground truth".format(current_subject)
-
-            prediction = os.path.join(preds_path, folders)
-
-            for each_pred in all_subjects_preds:
-                if folders in each_pred:
-                    prediction_file = os.path.join(preds_path, each_pred)
-                    break
-                    
-            # generate metrics
-class ACC:
-    # Given this is a toy example, the metric is implemented by hand
-    # It is recommended that metrics are obtained from trusted
-    # libraries
-    @staticmethod
-    def run(labels, preds):
-        total_count = len(labels)
-        correct_count = (labels == preds).sum()
-        return correct_count / total_count
+def score(parent, pred_lst, tmp_output="tmp.csv") -> pd.DataFrame:
+    """Compute and return scores for each scan."""
+    scores = []
+    for pred in pred_lst:
+        subject_id = pred.replace(".nii.gz", "")
+        gold = os.path.join(parent, subject_id, subject_id + "_seg.nii.gz")
+        try:
+            run_captk(pred, gold, tmp_output)
+            scan_scores = extract_metrics(tmp_output, subject_id)
+            os.remove(tmp_output)  # Remove file, as it's no longer needed
+        except subprocess.CalledProcessError:
+            # If no output found, give penalized scores.
+            scan_scores = (
+                pd.DataFrame({
+                    "subject_id": [subject_id],
+                    "Dice_ET": [0], "Dice_TC": [0], "Dice_WT": [0],
+                    "Hausdorff95_ET": [374], "Hausdorff95_TC": [374],
+                    "Hausdorff95_WT": [374], "Sensitivity_ET": [0],
+                    "Sensitivity_TC": [0], "Sensitivity_WT": [0],
+                    "Specificity_ET": [0], "Specificity_TC": [0],
+                    "Specificity_WT": [0], "Precision_ET": [0],
+                    "Precision_TC": [0], "Precision_WT": [0]
+                })
+                .set_index("subject_id")
+            )
+        scores.append(scan_scores)
+    return pd.concat(scores).sort_values(by="subject_id")
 
 
 def main():
@@ -124,10 +100,9 @@ def main():
     args = parser.parse_args()
 
     # Load all files
-    results = generate_stats(args.data_path, args.preds_dir)
+    results = score(args.data_path, os.listdir(args.preds_dir))
 
-    with open(args.output_file, "w") as f:
-        yaml.dump(results, f)
+    results.to_csv(args.output_file)
 
 
 if __name__ == "__main__":
